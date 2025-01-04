bbb3c4423b6069dee7d30550e4185e293327c603
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2025-01-05 02:34:32 +0600

    Updated feature X and fixed bug Y

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Component/Header'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Component/Home'
-import Cart from './Component/Cart'
+import "./App.css";
+import Header from "./Component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Component/Home";
+import Cart from "./Component/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,68 +1,45 @@
 import { createContext, useReducer } from "react";
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 import { Cartreducer, productReducer } from "./reducer";
 
-const cartcontext = createContext()
+const cartcontext = createContext();
 
-const Stock = [0, 3, 5, 6, 7]
-const Rating = [1, 2, 3, 4, 5]
-faker.seed(99)
+const Stock = [0, 3, 5, 6, 7];
+const Rating = [1, 2, 3, 4, 5];
+faker.seed(99);
 const ContextProvider = ({ children }) => {
+  const products = [...Array(100)].map(() => ({
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.url(),
+    inStock: Math.floor(Math.random() * Stock.length),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: Math.floor(Math.random() * Rating.length),
+  }));
 
-    const products = [...Array(60)].map(() => ({
-        id: faker.string.uuid(),
-        name: faker.commerce.productName(),
-        price: faker.commerce.price(),
-        image: faker.image.url(),
-        inStock: Math.floor(Math.random() * Stock.length),
-        fastDelivery: faker.datatype.boolean(),
-        ratings: Math.floor(Math.random() * Rating.length)
-    }));
+  const [state, dispatch] = useReducer(Cartreducer, {
+    products: products,
+    cart: [],
+  });
 
-    const [state, dispatch] = useReducer(Cartreducer, {
-        products: products,
-        cart: []
-    });
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
 
-    const [productState, productDispatch] = useReducer(productReducer, {
-        byStock: false,
-        byFastDelivery: false,
-        byRating: 0,
-        searchQuery: "",
-    });
-
-    return <cartcontext.Provider value={{ state, dispatch, productState, productDispatch }}>
-        {children}
+  return (
+    <cartcontext.Provider
+      value={{ state, dispatch, productState, productDispatch }}
+    >
+      {children}
     </cartcontext.Provider>
-}
+  );
+};
 
-export { ContextProvider, cartcontext }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export { ContextProvider, cartcontext };
 
 // productId: faker.string.uuid(),
 // productName: faker.commerce.productName(),
